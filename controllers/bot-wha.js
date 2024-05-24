0c8691d3339e524aa556ea8652eb047e64af4501
victor morales <63662613+santiago2001-dev@users.noreply.github.com>
2024-05-24 08:37:20 -0500

    fix ajuste autenticacion

--- controllers/bot-wha.js
+++ controllers/bot-wha.js
@@ -1,188 +1,184 @@
-const {client} = require('../middelwares/conexion-wha');
-const Producto = require('./curd-producto')
+const { client } = require("../middelwares/conexion-wha");
+const Producto = require("./curd-producto");
+
 class Menu {
+  async opciones() {
+    this.menuPrincipal();
 
- 
-      async opciones (){
+    client.on("message", async (message) => {
+      switch (message.body) {
+        case "1":
+          const producto = new Producto();
+          let result = await producto.getAllProduct();
+          await client.sendMessage(message.from, result);
+          this.menuPrincipal();
+          break;
 
+        case "2":
+          await client.sendMessage(
+            message.from,
+            "Por favor ingresa el nombre, precio y cantidad del producto separado por coma"
+          );
+          this.create();
+          break;
 
-        this.menuPrincipal();
+        case "3":
+          await client.sendMessage(
+            message.from,
+            "Ingresa el nombre del producto, el precio y la cantidad a actualizar, separados por coma"
+          );
+          this.update();
+          break;
 
-        client.on('message',async message=>{
-                  if(message.body == '1'){
-                    const producto = new Producto();
+        case "4":
+          await client.sendMessage(
+            message.from,
+            "Ingresa el nombre del producto para eliminar"
+          );
+          this.delete();
+          break;
 
-                    let result = await producto.getAllProduct()
-                     await client.sendMessage(message.from,result);
-              
-                    
-                      this.menuPrincipal();
-
-                   }
-                     if(message.body =='2'){
-                      await  client.sendMessage(message.from,'porfavor ingresa el nombre , precio y cantidad del producto seperado por coma');
-                      this.create()
-                    
-                     
-
-                    }
-                    if(message.body =='3'){
-                      await client.sendMessage(message.from ,'Ingresa el nombre del producto seperado por coma el precio y cantidad a actualizar')
-                      this.update()
-                    }
-                  if(message.body =='4'){
-                    await client.sendMessage(message.from,'porfavor ingresa el nombre del producto a eliminar')
-
-                    this.delete()
-                  }
-              })
-           
-}
-
-        menuPrincipal(){
-          client.on('message', message=>{
-          if(message.body =='hola'  && !message.body.includes  ('1') && !message.body.includes  ('2')&& !message.body.includes  ('3')&& !message.body.includes('4')){
-
-                client.sendMessage(message.from,
-                  'Hola soy bootStore Poli , ¿en que te puedo colaborar? 😀👋\n Estas son las opciones \n'+
-                  '1.🔍 Ver lista de productos \n'+
-                  '2. ➕ Agregar Productos \n'+
-                  '3.🔁 Actualizar Producto \n'
-                  +'4.✖ Borrar Producto \n'
-              );
-                  }
-                  })
-                }
-
-           
-           
-      getall(){
-        const producto = new Producto();
-        client.on('message', async message => {
-          try {
-            console.log(message.body);
-            const name = message.body.trim();
-            if (name == '1' ||name == '2' ||name == '3' ||name == '4' ) {
-              this.menuPrincipal()
-             } else {
-              const result = await producto.getAllProduct();
-              client.sendMessage(message.from, result);
-            }
-          } catch (error) {
-            console.error(error);
-            client.sendMessage(message.from, 'No se pudo obtener los productos');
-          } finally{
-            return
-          }
-        })
+        default:
+          await client.sendMessage(
+            message.from,
+            "de vuelta en el menu principal"
+          );
+          this.menuPrincipal();
+          break;
       }
-      create(){
-      const producto = new Producto();
+    });
+  }
 
-      client.on('message',async message => {
-      let array = message.body.split(",");
-        let nameProduct = array[0];
-        let priceProduct = array[1];
-        let stockProduct = array[2];
-
-        try{
-           const product = message.body.trim();
-           if (product == '1' ||product == '2' ||product == '3' ||product == '4' ) {
-            this.menuPrincipal();
-           }else{
-            let resultado = await producto.addProduct(nameProduct,priceProduct,stockProduct)
-            client.sendMessage(message.from,resultado)
-           }
-        
-
-          }catch(error){
-            console.log(error)
-            client.sendMessage(message.from,'No se pudo agregar el producto')
-          }finally{
-            return
-          }
-      
-    })
-    }
-
-
-    delete() {
-        const producto = new Producto();
-        client.on('message', async message => {
-          try {
-            console.log(message.body);
-            const name = message.body.trim();
-            if (name == '1' ||name == '2' ||name == '3' ||name == '4' ) {
-              this.menuPrincipal()
-             } else {
-              const result = await producto.deleteProduct(name);
-              client.sendMessage(message.from, result);
-            }
-          } catch (error) {
-            console.error(error);
-            client.sendMessage(message.from, 'No se pudo eliminar el producto');
-          } finally{
-            return
-          }
-        })
+  menuPrincipal() {
+    client.on("message", (message) => {
+      if (
+        message.body.toLowerCase === "hola" &&
+        !message.body.includes("1") &&
+        !message.body.includes("2") &&
+        !message.body.includes("3") &&
+        !message.body.includes("4")
+      ) {
+        client.sendMessage(
+          message.from,
+          "Hola soy bootStore Poli , ¿en que te puedo colaborar? 😀👋\n Estas son las opciones \n" +
+            "1.🔍 Ver lista de productos \n" +
+            "2. ➕ Agregar Productos \n" +
+            "3.🔁 Actualizar Producto \n" +
+            "4.✖ Borrar Producto \n"
+        );
       }
-      
+    });
+  }
 
-    update(){
-      const producto = new Producto();
-      client.on('message', async message => {
-        let array = message.body.split(",");
-        let nameProduct = array[0];
-        let priceProduct = array[1];
-        let stockProduct = array[2];
+  getAll() {
+    const producto = new Producto();
+    client.on("message", async (message) => {
+      try {
+        console.log(message.body);
         const name = message.body.trim();
-        try{
-          if (name == '1' ||name == '2' ||name == '3' ||name == '4' ) {
-            this.menuPrincipal()
-          }else{
-            let resultado = await producto.updateProduct(nameProduct,priceProduct,stockProduct)
-            client.sendMessage(message.from,resultado)
-          }
-        }catch(error){
-            console.log(error)
-            client.sendMessage(message.from,'No se pudo actualizar el producto')
-        }finally{
-          return
+        if (name == "1" || name == "2" || name == "3" || name == "4") {
+          this.menuPrincipal();
+        } else {
+          const result = await producto.getAllProduct();
+          client.sendMessage(message.from, result);
         }
-      })
-    }
-   
+      } catch (error) {
+        console.error(error);
+        client.sendMessage(message.from, "No se pudo obtener los productos");
+      } finally {
+        client.removeAllListeners("message"); // Termina el ciclo del mensaje
+        this.menuPrincipal();
+      }
+    });
+  }
+
+  create() {
+    const producto = new Producto();
+
+    client.on("message", async (message) => {
+      let array = message.body.split(",");
+      let nameProduct = array[0];
+      let priceProduct = array[1];
+      let stockProduct = array[2];
+
+      try {
+        const product = message.body.trim();
+        if (
+          product == "1" ||
+          product == "2" ||
+          product == "3" ||
+          product == "4"
+        ) {
+          this.menuPrincipal();
+        } else {
+          let resultado = await producto.addProduct(
+            nameProduct,
+            priceProduct,
+            stockProduct
+          );
+          client.sendMessage(message.from, resultado);
+        }
+      } catch (error) {
+        console.log(error);
+        client.sendMessage(message.from, "No se pudo agregar el producto");
+      } finally {
+        client.removeAllListeners("message"); // Termina el ciclo del mensaje
+        this.menuPrincipal();
+      }
+    });
+  }
+
+  delete() {
+    const producto = new Producto();
+    client.on("message", async (message) => {
+      try {
+        console.log(message.body);
+        const name = message.body.trim();
+        if (name == "1" || name == "2" || name == "3" || name == "4") {
+          this.menuPrincipal();
+        } else {
+          const result = await producto.deleteProduct(name);
+          client.sendMessage(message.from, result);
+        }
+      } catch (error) {
+        console.error(error);
+        client.sendMessage(message.from, "No se pudo eliminar el producto");
+      } finally {
+        client.removeAllListeners("message"); // Termina el ciclo del mensaje
+        this.menuPrincipal();
+      }
+    });
+  }
+
+  update() {
+    const producto = new Producto();
+    client.on("message", async (message) => {
+      let array = message.body.split(",");
+      let nameProduct = array[0];
+      let priceProduct = array[1];
+      let stockProduct = array[2];
+      const name = message.body.trim();
+      try {
+        if (name == "1" || name == "2" || name == "3" || name == "4") {
+          this.menuPrincipal();
+        } else {
+          let resultado = await producto.updateProduct(
+            nameProduct,
+            priceProduct,
+            stockProduct
+          );
+          client.sendMessage(message.from, resultado);
+        }
+      } catch (error) {
+        console.log(error);
+        client.sendMessage(message.from, "No se pudo actualizar el producto");
+      } finally {
+        client.removeAllListeners("message"); // Termina el ciclo del mensaje
+        this.menuPrincipal();
+      }
+    });
+  }
 }
 
-module.exports = Menu
+module.exports = Menu;
 
-//  //leer mensajes
-//   client.on('message', async (message) => {
-//   console.log(message.body)
-//   })
-
-
-//   //responder mensaje
-//   client.on('message', message => {
-//   	if(message.body === '!ping') {
-//   		message.reply('pong');
-//   	}
-//   });
-
-
-//   //enviar mensaje a numero especifico
-//  client.on('message', message => {
-//      let chatID = codigo_pais + number + "@c.us";
-
-//  	msj ="hola"
-
-//  		client.sendMessage(chatID,msj);
-	
-//  });
-
-//   //enviar mensaje a cualquier numero segun el cuerpo de el mensaje
-//  client.on('message', message => {
-//  	if(message.body === '!ping') {
-//  		client.sendMessage(message.from, 'pong');
-//  	}
-//  });
--- middelwares/conexion-wha.js
+++ middelwares/conexion-wha.js
@@ -1,22 +1,32 @@
- const qrcode = require('qrcode-terminal');
-
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const { Client,LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 
 
+const client = new Client({
+  authStrategy: new LocalAuth({
+    dataPath: "sessions",
+  }),
+  puppeteer: {},
+  headless: true,
+  args: ["--no-sandbox"],
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  },
+});
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
- });
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.initialize().catch((err) => console.log(err));
 
 
- client.on('ready', () => {
-     console.log('Client is ready!');
- });
-
- client.initialize();
-
- 
-
- module.exports = {client};
-
+module.exports = {
+  client,
+};
